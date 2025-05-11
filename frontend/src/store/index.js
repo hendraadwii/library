d4db13a456fd09efc578f9c996314e2710a51f11
Hendra Turut Dwi Laksono <h.dwiaxa@gmail.com>
2025-05-12 01:10:48 +0700

    Vuex store configuration

---
+++ frontend/src/store/index.js
@@ -0,0 +1,44 @@
+import { createStore } from 'vuex'
+import auth from './modules/auth'
+import books from './modules/books'
+import borrowings from './modules/borrowings'
+import users from './modules/users'
+
+export default createStore({
+  state: {
+    loading: false,
+    error: null
+  },
+  mutations: {
+    SET_LOADING(state, loading) {
+      state.loading = loading
+    },
+    SET_ERROR(state, error) {
+      state.error = error
+    },
+    CLEAR_ERROR(state) {
+      state.error = null
+    }
+  },
+  actions: {
+    setLoading({ commit }, loading) {
+      commit('SET_LOADING', loading)
+    },
+    setError({ commit }, error) {
+      commit('SET_ERROR', error)
+    },
+    clearError({ commit }) {
+      commit('CLEAR_ERROR')
+    }
+  },
+  getters: {
+    loading: state => state.loading,
+    error: state => state.error
+  },
+  modules: {
+    auth,
+    books,
+    borrowings,
+    users
+  }
+}) 
