17db7fa9bcdff8a3fc1b9894404e41141b99c653
Hendra Turut Dwi Laksono <h.dwiaxa@gmail.com>
2025-05-12 00:50:38 +0700

    Titik masuk utama server API, menginisialisasi dan menjalankan server HTTP

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/aksaaaraa/library/internal/server"
+)
+
+func main() {
+	// This is just a proxy to the actual server implementation
+	// in server.go to allow better code organization
+	server.Run()
+}
